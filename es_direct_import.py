b8d728d5c705d16a21018b3697ab4b65472d2754
Russell Troxel <russell.troxel@rackspace.com>
2015-09-24 13:56:42 -0500

    updates

--- es_direct_import.py
+++ es_direct_import.py
@@ -2,13 +2,11 @@
 
 from __future__ import print_function
 import logging
-import pymongo
 import sys
 import yaml
 
 from elasticsearch import Elasticsearch
 from pymongo import MongoClient
-from pymongo.cursor import CursorType
 from time import time
 
 
@@ -36,47 +34,34 @@ def connect_mongo():
     c = conf["elasticsearch"]
     db = MongoClient(c["mongohost"], c["mongoport"])
     db.admin.authenticate(
-        c["mongouser"],
-        c["mongopass"],
+        "swyytch",
+        "3L9t8KeqQ3I96^M159^@93O3PCZV*4X7aSEmlZrTHzkGHe^F",
         mechanism='SCRAM-SHA-1'
     )
     return db
 
 
-def prepare(entry):
-    dbname, colname = entry["ns"].split(".")
-
-    # If an insert, set rec to inserted rec
-
-    if entry["op"] == 'i':
-        rec = entry["o"]
-
-    # If an update, set rec to retrieved rec
-    elif entry["op"] == "u":
-        query = {"_id": doc["o2"]["_id"]}
-        rec = db[dbname][colname].find(query)[0]
-    else:
-        return False
+def prepare(rec, dbname, collection):
     rec["timestamp"] = time()
     rec["_id"] = str(rec["_id"])
     if "parent" in rec:
         rec["parent"] = str(rec["parent"])
 
-    return {"body": rec, "db": dbname, "col": colname}
+    return {"body": rec, "db": dbname, "col": collection}
 
 
 def get_es_values(rec):
-    if rec["db"] == "bernie":
-        name = [rec["col"], rec["body"]["lang"], VERSION]
-        rec["es_index"] = "_".join(name)
-        rec["es_type"] = rec["body"]["site"].replace(".", "_")
-    elif rec["db"] == "facebook":
-        if rec["col"] == "token":
-            return False
-        name = ["fb", rec["body"]["page"], VERSION]
-        rec["es_index"] = "_".join(name)
-        rec["es_type"] = rec["col"] if rec["col"] != "data" else "stats"
-    elif rec["db"] == "campaign":
+    # if rec["db"] == "bernie":
+    #     name = [rec["col"], rec["body"]["lang"], VERSION]
+    #     rec["es_index"] = "_".join(name)
+    #     rec["es_type"] = rec["body"]["site"].replace(".", "_")
+    # elif rec["db"] == "facebook":
+    #     if rec["col"] == "token":
+    #         return False
+    #     name = ["fb", rec["body"]["page"], VERSION]
+    #     rec["es_index"] = "_".join(name)
+    #     rec["es_type"] = rec["col"] if rec["col"] != "data" else "stats"
+    if rec["db"] == "campaign":
         name = ["campaign_events", VERSION]
         rec["es_index"] = "_".join(name)
         rec["es_type"] = rec["col"]
@@ -89,23 +74,19 @@ if __name__ == '__main__':
     db = connect_mongo()
     conf = config()["elasticsearch"]
     es = Elasticsearch(conf["host"])
-    oplog = db.local.oplog.rs
-    first = next(oplog.find().sort('$natural', pymongo.DESCENDING).limit(-1))
-    ts = first['ts']
-
-    while True:
-        cursor = oplog.find(
-            {'ts': {'$gt': ts}},
-            cursor_type=CursorType.TAILABLE_AWAIT,
-            oplog_replay=True
-        )
-        while cursor.alive:
+    for dbname in ["bernie", "facebook"]:
+        for collection in db[dbname].collection_names():
+            if collection in ["system.indexes"]:
+                continue
+            cursor = db[dbname][collection].find()
             for doc in cursor:
-                ts = doc['ts']
-                rec = prepare(doc)
-                if rec:
-                    rec = get_es_values(rec)
+                rec = prepare(doc, dbname, collection)
                 if not rec:
+                    continue
+                rec = get_es_values(rec)
+                if not rec:
+                    continue
+                if rec["col"] == "events":
                     continue
                 if "parent" in rec["body"]:
                     es.index(
